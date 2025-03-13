38425ed4a3098a988d8d6350b4756455ac019370
SENGALOUN-LORVANH <sane.slv9797@gmail.com>
2025-03-13 13:58:18 +0700

    update

--- app/about/error.tsx
+++ app/about/error.tsx
@@ -1,12 +1,20 @@
-'use client'
-import React from 'react'
+"use client";  
+import React from "react";
 
-const Error = () => {
+const ErrorPage = ({ error, reset }: { error: Error; reset: () => void }) => {
+    console.error("Error fetching data:", error);
   return (
-    <div>
-      <h1>There's something went wrong!</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-3xl font-bold text-red-600">Mun mee ERROR!!!</h1>
+      <p className="text-gray-600">{error.message}</p>
+      <button
+        onClick={reset}
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md"
+      >
+        Try Again
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Error
+export default ErrorPage;
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const url = "https://jsonplaceholder.typicode.com/todos";
 
 type Todo = {
@@ -6,43 +8,20 @@ type Todo = {
   completed: boolean;
 };
 
-const fetchData = async (): Promise<Todo[]> => {
-  try {
-    await new Promise(resolve => setTimeout(resolve, 500)); 
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch data");
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return []; 
-  }
-};
-
-export default async function AboutPage() { 
-  const data: Todo[] = await fetchData();
+const About = async () => {
+  const res = await fetch(url);
+  const todos: Todo[] = await res.json();
 
   return (
     <div>
-      <main>
-        <header>
-          <h1>About us</h1>
-        </header>
-
-        {data.length > 0 ? (
-          data.map((item) => (
-            <div key={item.id}>
-              <h2>{item.title}</h2>
-              <p>{item.completed ? "Completed" : "Not Completed"}</p>
-            </div>
-          ))
-        ) : (
-          <p>Loading or No Data Available...</p>
-        )}
-      </main>
+      <h1>About Page</h1>
+      <ul>
+        {todos.map((item) => (
+          <li key={item.id}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
+
+export default About;
